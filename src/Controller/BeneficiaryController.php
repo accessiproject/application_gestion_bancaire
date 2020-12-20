f97351fb7e9ca31e2aaaf79f48ea36c0120551c3
accessiproject <kevin.bustamante@maiil.novancia.fr>
2020-12-20 05:54:37 +0100

    creation new method beneficiary

--- src/Controller/BeneficiaryController.php
+++ src/Controller/BeneficiaryController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Beneficiary;
 use App\Repository\BeneficiaryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\BeneficiaryType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,12 +19,43 @@ class BeneficiaryController extends AbstractController
      */
     public function beneficiary_index()
     {
-		$beneficiaries = $this->getDoctrine()->getRepository(Beneficiary::class)->findByUser(84);
+		$user = $this->getUser();
+		$beneficiaries = $this->getDoctrine()->getRepository(Beneficiary::class)->findByUser($user->getId());
         return $this->render('beneficiary/index.html.twig', [
             'controller_name' => 'BeneficiaryController',
 			'beneficiaries' => $beneficiaries,
         ]);
     }
+	
+	/**
+     * @Route("/customer/beneficiary/new", name="beneficiary_new")
+     */
+    public function beneficiary_new(Request $request): Response
+    {
+        
+        $beneficiary = new Beneficiary();
+        $form = $this->createForm(BeneficiaryType::class, $beneficiary);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+			$user = $this->getUser();
+			$beneficiary->setUser($user);
+            $beneficiary->updatedTimestamps();
+            $entityManager->persist($beneficiary);
+            $entityManager->flush();    
+            
+            return $this->redirectToRoute('beneficiary_list', [
+            'id' => $user->getId(),
+        ]);
+        }
+
+        return $this->render('beneficiary/new.html.twig', [
+            'beneficiary' => $beneficiary,
+            'form' => $form->createView(),
+        ]);
+    }
+	
 	
 		    /**
      * @Route("/customer/beneficiary/delete/{id}", name="beneficiary_delete")
--- src/Form/BeneficiaryType.php
+++ src/Form/BeneficiaryType.php
@@ -6,19 +6,34 @@ use App\Entity\Beneficiary;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class BeneficiaryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('iban')
-            ->add('updatedat')
-            ->add('createdat')
-            ->add('user')
-        ;
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'required' => true,
+                    'placeholder' => 'Prénom',
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'required' => true,
+                    'placeholder' => 'Nom',
+                ],
+            ])
+            ->add('iban', TextType::class, [
+                'label' => 'N° iban',
+                'attr' => [
+                    'required' => true,
+                    'placeholder' => 'N° iban',
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
